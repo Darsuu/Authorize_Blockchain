39f72c059d23921c2d1f4b858f35d709868a9d73
Darshan <darshanabhay@hotmail.com>
2023-02-23 17:31:48 +0530

    TODO: Add Mnemonic passing and token transfer

--- client/server.js
+++ client/server.js
@@ -3,10 +3,15 @@ const fs = require('fs');
 var request = require('request');
 var cors = require("cors");
 var bodyParser = require("body-parser");
+const proto = require('@cosmjs/proto-signing');
+const crypto = require('crypto');
+const readline = require('readline');
 const {Secp256k1HdWallet} = require("@cosmjs/launchpad")
 
 const HOST_NAME = '127.0.0.1';
 const PORT = 4000;
+const algorithm = 'aes-256-cbc';
+const iv = crypto.randomBytes(16);
 
 const app = express();
 
@@ -15,19 +20,18 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
   }));
 
+app.use(express.static('views'));
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
 
 app.use(cors());
 
-(async() =>{
-  
-  
-})();
-
-app.post('/estimate', async (req, res) => {
+app.post('/activate', async (req, res) => {
   const wallet = await Secp256k1HdWallet.generate(12, {prefix: "cosmos"});
   const [{ address }] = await wallet.getAccounts();
+  // console.log(encrypt.encryptedData);
+  // const mnemonic = await decrypt(encrypted);
+  // console.log(mnemonic);
   console.log("Address:", address);
   request({
     url: "http://127.0.0.1:4001/receive",
@@ -38,8 +42,87 @@ app.post('/estimate', async (req, res) => {
   res.end();
 });
 
+async function decrypt(text) {
+  let iv = Buffer.from(text.iv, 'hex');
+  let encryptedText = Buffer.from(text.encryptedData, 'hex');
+  let decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
+  let decrypted = decipher.update(encryptedText);
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+  return decrypted.toString();
+}
+
+async function createPassFile(pass)
+{
+  const wallet = await proto.DirectSecp256k1HdWallet.generate(24);
+  console.log(wallet.mnemonic);
+  var key1 = crypto.createHash('sha256').update(String(pass)).digest('hex').substring(0, 32);
+  var encrypted = await encrypt(wallet.mnemonic, key1);
+  fs.writeFile('pass.txt', encrypted.encryptedData + "\n" + key1, function (err) {
+  if (err) throw err;
+  console.log('File is created successfully.');
+  });
+}
+
+async function encrypt(text, key) {
+  let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
+  let encrypted = cipher.update(text);
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+  return { iv: iv.toString('hex'),
+  encryptedData: encrypted.toString('hex') };
+}
+
+app.post('/login', async (req, res) => {
+  console.log("Login request received");
+  const pass = crypto.createHash('sha256').update(String(req.body.pass)).digest('hex').substring(0, 32);
+  const fileStream = fs.createReadStream('./pass.txt');
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+  const credentials = [];
+  for await (const line of rl) {
+    credentials.push(line);
+  }
+  if(credentials[1] === pass)
+  {
+    console.log("Login successful");
+    res.end();
+  } 
+  else console.log("Login failed");
+  
+});
+
+app.post('/signup', async (req, res) => {
+  const pass = req.body.pass;
+  const confirmpass = req.body.confirmPass;
+  if(pass === confirmpass)
+  {
+      createPassFile(pass);
+      console.log("Successful");
+      res.end();
+  }
+  else console.log("Password and Confirm Password must be equal");
+});
+
+app.get('/logged-in', async (req, res) => {
+  html = fs.readFileSync('./views/logged-in.html');
+  res.write(html);
+  res.end();
+});
+
 app.get('/', (req, res) => {
-  html = fs.readFileSync('./views/home.html');
+  var html;
+  if(fs.existsSync("./pass.txt"))
+  {
+      html = fs.readFileSync('./views/login.html');
+  } 
+  else html = fs.readFileSync('./views/signup.html');
+  res.write(html);
+  res.end();
+});
+
+app.get('/login', (req, res) => {
+  html = fs.readFileSync('./views/login.html');
   res.write(html);
   res.end();
 });
--- trustedentity/server.js
+++ trustedentity/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const proto = require('@cosmjs/proto-signing');
 const stargate = require('@cosmjs/stargate');
-
+const dotenv = require('dotenv');
+dotenv.config();
+module.exports = {
+  ADDRESS_OF_TRUSTED_ENTITY: process.env.ADDRESS_OF_TRUSTED_ENTITY,
+  MNEMONIC_OF_TRUSTED_ENTITY: process.env.MNEMONIC_OF_TRUSTED_ENTITY,
+};
 var cors = require("cors");
 var bodyParser = require("body-parser");
 
-const RPC_END_POINT = "http://192.168.56.1:26657";  //wsl
+const RPC_END_POINT = "http://172.22.42.11:26657";  //wsl
 const HOST_NAME = '127.0.0.1';
 const PORT = 4001;
 
@@ -22,14 +27,18 @@ app.use(express.urlencoded()); // to support URL-encoded bodies
 app.use(cors());
 
 app.post('/receive', async (req, res) => {
+  //console.log(res.body.mnemonic);
   await account_creation_tx(req.body.address);
+  res.end();
 });
 
 async function account_creation_tx(reciever_address)
 {
   try {
-      // const guardian_auth_address = <ADDRESS OF TRUSTED ENTITY>
-      // const guardian_auth_mnemonic = <MNEMONIC OF TRUSTED ENTITY>
+      const guardian_auth_address = process.env.ADDRESS_OF_TRUSTED_ENTITY
+      const guardian_auth_mnemonic = process.env.MNEMONIC_OF_TRUSTED_ENTITY
+      // const guardian_auth_address = ""
+      // const guardian_auth_mnemonic = ""
       const guardian_auth_wallet = await proto.DirectSecp256k1HdWallet.fromMnemonic(guardian_auth_mnemonic);
       const guardian_auth = await stargate.SigningStargateClient.connectWithSigner(RPC_END_POINT, guardian_auth_wallet);
       console.log("Requester's address: ", reciever_address);
@@ -42,7 +51,6 @@ async function account_creation_tx(reciever_address)
         ],
         gas: "250000",
       };
-      console.log("Reached");
       const result = await guardian_auth.sendTokens(guardian_auth_address, reciever_address, [{denom: "token", amount: "100"}], fee);
       console.log(result);
       stargate.assertIsDeliverTxSuccess(result);
